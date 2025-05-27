9c7cbd45662620729b6a4efdeb99162a3a8c9dfc
Lê Hùng <lhungdayy@gmail.com>
2025-05-27 23:03:36 +0700

    update login feature

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\AuthUser;
+use App\Http\Middleware\AuthAdmin;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard',[UserController::class,'index'])->name('user.index');
+});
+Route::middleware(['auth', AuthAdmin::class])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+});
